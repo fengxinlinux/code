c2139b83fb633e2667e84d68a27567c3028bc6d6
fengxinlinux <903087053@qq.com>
2016-05-23 17:44:25 +0800

    axin

---
+++ axin.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: axin.c
+	> Author: FengXin
+	> Mail: fengxinlinux@gmail.com
+	> Created Time: 2016年05月23日 星期一 17时43分57秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<sys/signal.h>
+#include<unistd.h>
+
