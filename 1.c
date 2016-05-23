29acd1280a0efdee1e43c11cab7aff119baaf79f
fengxinlinux <903087053@qq.com>
2016-05-23 17:34:00 +0800

    a

--- 1.c
+++ 1.c
@@ -1,1 +1,14 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: FengXin
+	> Mail: fengxinlinux@gmail.com
+	> Created Time: 2016年05月23日 星期一 17时20分45秒
+ ************************************************************************/
+
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<sys/signal.h>
+#include<unistd.h>
+
