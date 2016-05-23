4725e148f54dfe0ce99fd180d5011814553d7612
fengxinlinux <903087053@qq.com>
2016-05-23 17:08:06 +0800

    o

---
+++ 3.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 3.c
+	> Author: FengXin
+	> Mail: fengxinlinux@gmail.com
+	> Created Time: 2016年05月23日 星期一 17时07分29秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<sys/signal.h>
+#include<unistd.h>
+int main()
+{}
