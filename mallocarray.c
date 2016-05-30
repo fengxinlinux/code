335e7a41b38ab72d21060e1d5df1813d8d4997c7
fengxinlinux <903087053@qq.com>
2016-05-30 20:38:15 +0800

    动态申请二维数组

---
+++ mallocarray.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: 2.c
+	> Author: FengXin
+	> Mail: fengxinlinux@gmail.com
+	> Created Time: 2016年05月29日 星期日 21时53分56秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+      int i,j;
+  //  申请一个3行2列的整型数组
+    int (*a)[2]=(int(*)[2])malloc(sizeof(int)*3*2);
+    for(i=0;i<3;i++)
+    for(j=0;j<2;j++)
+    {
+       
+       printf("%p\n",&a[i][j]);
+
+    } //输出数组每个元素地址，每个元素的地址是连续的
+    return 0;
+}
